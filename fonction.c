e39361c0f074ad5211a311cfc09eb4b751fe4f4f
akksel1 <94923018+akksel1@users.noreply.github.com>
2022-04-23 16:08:57 +0200

    MENUS

--- fonction.c
+++ fonction.c
@@ -1,21 +1,16 @@
 //
 // Created by louka on 06/04/2022.
-#include "fonction.h"
-
-int main_menu(){
-    printf("\t\t-- MENU --\n");
-    printf("1 - PARTIE I");
-    printf("2 - PARTIE II");
-    printf("3 - PARTIE III");
-}
 //
 
 #include "fonction.h"
+#include<conio.h>
+#include<stdio.h>
+
 
 
 //BOOL verif(int , int S[][]{
 
-int** creer_matrice(int size)
+int** create_matrix(int size)
 {
     int** M = (int**) malloc(size * sizeof(int*));
     for (int i = 0; i < size; i++)
@@ -73,30 +68,158 @@ void initialize_matrix(int TL,int**M){
         }
     }
 }
+
 BOOL compter_nombre_ligne(int TL,int**M)
 {
-
     int cpt;
     int un=0;
     int zero=0;
-  for (int i=0;i<TL;i++)
-  {
-      for (int j=0;j<TL;j++)
-      {
-          cpt=M[i][j];
-          if (cpt==1)
-          {
-              un=un+1;
-          }
-          if (cpt==0);
-          {
-              zero=zero+1;
-          }
-      }
-  }
-  if (un==zero)
-  {
-      return TRUE;
-  }
-  return FALSE;
+    for (int i=0;i<TL;i++)
+    {
+        for (int j=0;j<TL;j++)
+        {
+            cpt=M[i][j];
+            if (cpt==1)
+            {
+                un=un+1;
+            }
+            if (cpt==0);
+            {
+                zero=zero+1;
+            }
+        }
+    }
+    if (un==zero)
+    {
+        return TRUE;
+    }
+    return FALSE;
+
 }
+
+
+
+// FONCTIONS MENU
+
+void menu1(char* choice){       //MENU PRINCIPAL
+    int cpt=0;
+    printf("\n\t--  MENU PRINCIPAL --\n\n");
+    printf("1 - Resoudre manuelement une grille\n");
+    printf("2 - Resoudre automatiquement une grille\n");
+    printf("Q - QUITTER\n");
+    do {
+        if(cpt==0){
+            //On vide le buffer sinon print 2 fois
+            fflush(stdin);
+            printf("\nSaisir votre choix:\n");
+            cpt++;
+        }
+        else{
+            //On vide le buffer sinon print 2 fois
+            fflush(stdin);
+            printf("Saisie incorrect. Resaisir:\n");
+        }
+        scanf("%c",choice);
+    }while(((*choice>'2') || (*choice<'1')) && (*choice!='q') && (*choice!='Q'));
+
+    //
+    if(*choice=='1'){
+        menu1_1(choice);
+    }
+    /*if(*choice=='2'){
+    }
+     */
+}
+
+
+void menu1_1(char* choice){         //SOUS MENU 1_1
+    int cpt=0;
+    int ** M=NULL;
+
+    printf("\t--  RESOUDRE MANUELEMENT GRILLE --\n");
+    printf("1 - GRILLE 4x4\n");
+    printf("2 - GRILLE 8x8\n");
+    printf("3 - GRILLE 16x16\n");
+    printf("R - RETOUR\n");
+    do {
+        if(cpt==0){
+            //On vide le buffer sinon print 2 fois
+            fflush(stdin);
+            printf("\nSaisir votre choix:\n");
+            cpt++;
+        }
+        else{
+            //On vide le buffer sinon print 2 fois
+            fflush(stdin);
+            printf("Saisie incorrect. Resaisir:\n");
+        }
+        scanf("%c",choice);
+    }while(((*choice>'3') || (*choice<'1')) && (*choice!='R') && (*choice!='r'));
+
+    // GRILLE 4X4
+    if(*choice=='1'){
+        M=create_matrix(4);
+        menu1_2(4,choice,M);
+
+    }
+    // GRILLE 8X8
+    if(*choice=='2'){
+        M=create_matrix(8);
+        menu1_2(8,choice,M);
+    }
+    // GRILLE 16X16
+    if(*choice=='3'){
+        M=create_matrix(16);
+        menu1_2(16,choice,M);
+    }
+
+    // RETOUR AU MENU1
+    if((*choice=='r')||(*choice=='R')){
+        menu1(choice);
+    }
+}
+
+int menu1_2(int dim,char* choice,int** M){       //SOUS MENU 1_2
+    int cpt=0;
+    printf("\t--  RESOUDRE MANUELEMENT GRILLE %dx%d  --\n",dim,dim);
+    printf("1 - SAISIR MANUELEMENT UN MASQUE\n");
+    printf("2 - GENERER UN MASQUE\n");
+    printf("3 - JOUER\n"); //avec un masque aléatoire
+    printf("R - RETOUR\n");
+    do {
+        if(cpt==0){
+            //On vide le buffer sinon print 2 fois
+            fflush(stdin);
+            printf("\nSaisir votre choix:\n");
+            cpt++;
+        }
+        else{
+            //On vide le buffer sinon print 2 fois
+            fflush(stdin);
+            printf("Saisie incorrect. Resaisir:\n");
+        }
+        scanf("%c",choice);
+    }while(((*choice>'3') || (*choice<'1')) && (*choice!='R') && (*choice!='r'));
+
+    // SAISIE MANUELLE MASQUE(print grille jeu et masque)
+    /*if(*choice=='1'){
+
+    }
+    // GENERER UN MASQUE AUTO(print grille jeu et masque)
+    if(*choice=='2'){
+
+    }
+    // JOUER AVEC UN MASQUE AUTO
+    if(*choice=='3'){
+
+    }*/
+
+    // RETOUR AU MENU1_1
+    if((*choice=='r')||(*choice=='R')){
+        menu1_1(choice);
+    }
+
+
+}
+
+// FIN FONCTIONS MENU
--- main.c
+++ main.c
@@ -4,25 +4,15 @@
 
 int main()
 {
-// initialisation de la matrice en séquence
-    /*int M[4][4] = {};
-    int i, j, lig = 4, col = 4;
-    for (i = 0; i < lig; i++) {
-        for (j = 0; j < col; j++) {
-            printf("%d \t\t", M[i][j]); // séparer les valeursd 'une même ligne par 2 tabulations
-        }
-        printf("\n"); // Saut de ligne avant d'afficher la suivante
-    }
-*/
-
     int ** M=NULL;
-    BOOL T;
-    M = creer_matrice(TL1);
-    initialize_matrix(TL1,M);
-    print_matrix(M,TL1);
-    T=compter_nombre_ligne(TL1,M);
-    printf("%d",T);
-
+    char choice;
+    menu1(&choice);
+    if(((choice)!='Q')&& (choice!='q')) {
+        M= create_matrix(16);
+        initialize_matrix(16, M);
+        print_matrix(M, 16);
+        getch(); //Pour ne pas arrêter le programme automatiquement (Conio.h)
+    }
     return 0;
 
 }
